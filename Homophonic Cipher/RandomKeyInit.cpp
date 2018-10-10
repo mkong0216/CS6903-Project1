c42fc9afe073609f1f6656c5652ea713f8c9ac12
nlm42 <nmawby@gmail.com>
2018-10-09 22:50:20 -0700

    nlm42 fixed errors

--- Homophonic Cipher/RandomKeyInit.cpp
+++ Homophonic Cipher/RandomKeyInit.cpp
@@ -5,118 +5,133 @@
 //  Created by nlm330 on 10/6/18.
 
 #include "RandomKeyInit.hpp"
+#include <tuple>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <array>
 
+using namespace std;
+
+//function to sort vector by third position, descending. used code from
+//https://www.geeksforgeeks.org/sorting-vector-tuple-c-descending-order/
+bool sortbyth(const tuple<int, char, float>& a, const tuple<int, char, float>& b) {
+  return (get<2>(a) > get<2>(b));
+}
 
 //the putative key out is the output value
 //the value inside the key should be 0-26
 //0 stands for space, 1-26 stands for a-z
 void RandomKeyInit::GetPutativeKey(vector<int> ciphertextFreq, int* putativeKeyOut){
-//    int putativeKey[106]={0};
-//  vector<float> ciphertextFreqPerc={0};
-//  float plaintextFreqPerc[27]={0};
-//  int sum=0;
-//  int i=0;
-//  float tempBest=10000;
-//  int tempBestPos=0;
-//
-//  // hardcoded vecor of frequeny values given to us
-//  //tuple<char_letter, char_count, char_perc
-//  vector<tuple<int, char, float>> plaintextFreq;
-//  givenTableFreq.push_back(make_tuple(char(32), 19, 0);
-//  givenTableFreq.push_back(make_tuple('a', 7, 0);
-//  givenTableFreq.push_back(make_tuple('b', 1, 0);
-//  givenTableFreq.push_back(make_tuple('c', 2, 0);
-//  givenTableFreq.push_back(make_tuple('d', 4, 0);
-//  givenTableFreq.push_back(make_tuple('e', 10, 0);
-//  givenTableFreq.push_back(make_tuple('f', 2, 0);
-//  givenTableFreq.push_back(make_tuple('g', 2, 0);
-//  givenTableFreq.push_back(make_tuple('h', 5, 0);
-//  givenTableFreq.push_back(make_tuple('i', 6, 0);
-//  givenTableFreq.push_back(make_tuple('j', 1, 0);
-//  givenTableFreq.push_back(make_tuple('k', 1, 0);
-//  givenTableFreq.push_back(make_tuple('l', 3, 0);
-//  givenTableFreq.push_back(make_tuple('m', 2, 0);
-//  givenTableFreq.push_back(make_tuple('n', 6, 0);
-//  givenTableFreq.push_back(make_tuple('o', 6, 0);
-//  givenTableFreq.push_back(make_tuple('p', 2, 0);
-//  givenTableFreq.push_back(make_tuple('q', 1, 0);
-//  givenTableFreq.push_back(make_tuple('r', 5, 0);
-//  givenTableFreq.push_back(make_tuple('s', 5, 0);
-//  givenTableFreq.push_back(make_tuple('t', 7, 0);
-//  givenTableFreq.push_back(make_tuple('u', 2, 0);
-//  givenTableFreq.push_back(make_tuple('a', 7, 0);
-//  givenTableFreq.push_back(make_tuple('v', 1, 0);
-//  givenTableFreq.push_back(make_tuple('w', 2, 0);
-//  givenTableFreq.push_back(make_tuple('x', 1, 0);
-//  givenTableFreq.push_back(make_tuple('y', 2, 0);
-//  givenTableFreq.push_back(make_tuple('z', 1, 0);
-//
-//  //set percentages to plaintextFreq
-//  cout << "printing given table frequency by letter: " << "\n";
-//  for(i=0,i<givenTableFreq.size(),i++){
-//    get<2>(givenTableFreq[i]) = get<1>(givenTableFreq[i]) / 106;
-//    cout << get<0>(givenTableFreq[i]) << " "
-//      << get<1>(givenTableFreq[i]) << " "
-//      << get<2>(givenTableFreq[i]) << "\n";
-//    }
-//
-//  //sort plaintextFreq by percentage
-//  sort(givenTableFreq.begin(), givenTableFreq.end(), sortbyth);
-//  cout << "printing given table sorted by frequency: " << "\n";
-//  cout << get<0>(givenTableFreq[i]) << " "
-//    << get<1>(givenTableFreq[i]) << " "
-//    << get<2>(givenTableFreq[i]) << "\n";
-//
-//  //sum ciphertextFreq and change to percentages
-//  sum=0;
-//  for(i=0,i<ciphertextFreq.size(),i++){
-//    sum = sum + ciphertextFreq[i];
-//  }
-//  for (i=0,i<ciphertextFreq.size(),i++){
-//    ciphertextFreqPerc[i] = ciphertextFreq[i] / sum;
-//  }
-//
-//  //Prepare ciphtertext frequeny vector
-//  //<int cipher number, float cipher number freq perc
-//  vector<pair<int, float>> cipher_freq_search;
-//  for(i=0,i<ciphertextFreq.size(),i++){
-//    cipher_freq_search.push_back(make_pair(ciphertextFreq[i],ciphertextFreqPerc[i]));
-//  }
-//
-//  //generate key
-//  for(i=0,i<givenTableFreq.size(),i++){
-//    float j = get<2>(plaintextFreq[i] / get<1>(plaintextFreq[i]);
-//    //int l = get<1>(plaintextFreq[i]);
-//    char k = get<0>(plaintextFreq[i]);
-//    for(int l=0,l<get<1>(plaintextFreq[i]),l++){
-//
-//      //find the closest value to J in cipher text frequencies
-//      for(int m=0,m<cipher_freq_search.size(),m++){
-//        if(abs(J-get<1>(cipher_freq_search[m])<tempBest){
-//          tempBest=abs(J-get<1>(cipher_freq_search[m]);
-//          tempBestPos=get<0>(cipher_freq_search[m]);
-//        }
-//
-//      //put value closest to J in the putativeKey array, remove vector position
-//      putativeKey[tempBestPos]=k;
-//      cipher_freq_search.erase(tempBestPos);
-//      }
-//
-//      //reset tempBest
-//      tempBest=10000;
-//      tempBestPos=0;
-//    }
-//  }
-//
-//  //print putativeKey[]
-//  cout << "The putative key is: " << "\n";
-//  for (i=0,i<putativeKey.size(),i++){
-//    cout << i << " " << putativeKey[i] >> "\n";
-//  }
-//  return putativeKey;
+  int putativeKey[105]={0};
+  vector<float> ciphertextFreqPerc={0};
+  float plaintextFreqPerc[27]={0};
+  int sum=0;
+  int i=0;
+  float tempBest=10000;
+  int tempBestPos=0;
+  float tempPerc=0;
+
+  // hardcoded vecor of frequeny values given to us
+  //tuple<char_letter, char_count, char_perc
+  vector<tuple<int, int, float>> plaintextFreq;
+  plaintextFreq.push_back(make_tuple(0, 19, 0));
+  plaintextFreq.push_back(make_tuple(1, 7, 0));
+  plaintextFreq.push_back(make_tuple(2, 1, 0));
+  plaintextFreq.push_back(make_tuple(3, 2, 0));
+  plaintextFreq.push_back(make_tuple(4, 4, 0));
+  plaintextFreq.push_back(make_tuple(5, 10, 0));
+  plaintextFreq.push_back(make_tuple(6, 2, 0));
+  plaintextFreq.push_back(make_tuple(7, 2, 0));
+  plaintextFreq.push_back(make_tuple(8, 5, 0));
+  plaintextFreq.push_back(make_tuple(9, 6, 0));
+  plaintextFreq.push_back(make_tuple(10, 1, 0));
+  plaintextFreq.push_back(make_tuple(11, 1, 0));
+  plaintextFreq.push_back(make_tuple(12, 3, 0));
+  plaintextFreq.push_back(make_tuple(13, 2, 0));
+  plaintextFreq.push_back(make_tuple(14, 6, 0));
+  plaintextFreq.push_back(make_tuple(15, 6, 0));
+  plaintextFreq.push_back(make_tuple(16, 2, 0));
+  plaintextFreq.push_back(make_tuple(17, 1, 0));
+  plaintextFreq.push_back(make_tuple(18, 5, 0));
+  plaintextFreq.push_back(make_tuple(19, 5, 0));
+  plaintextFreq.push_back(make_tuple(20, 7, 0));
+  plaintextFreq.push_back(make_tuple(21, 2, 0));
+  plaintextFreq.push_back(make_tuple(22, 7, 0));
+  plaintextFreq.push_back(make_tuple(23, 1, 0));
+  plaintextFreq.push_back(make_tuple(24, 2, 0));
+  plaintextFreq.push_back(make_tuple(25, 1, 0));
+  plaintextFreq.push_back(make_tuple(26, 2, 0));
+  plaintextFreq.push_back(make_tuple(27, 1, 0));
+
+  //set percentages to plaintextFreq
+  cout << "printing given table frequency by letter: " << "\n";
+  for(i=0;i<plaintextFreq.size();i++){
+    tempPerc = get<1>(plaintextFreq[i]);
+    tempPerc /= 106;
+    get<2>(plaintextFreq[i]) = tempPerc;
+    cout << get<0>(plaintextFreq[i]) << " "
+      << get<1>(plaintextFreq[i]) << " "
+      << get<2>(plaintextFreq[i]) << endl;
+    }
+
+  //sort plaintextFreq by percentage
+  sort(plaintextFreq.begin(), plaintextFreq.end(), sortbyth);
+
+  cout << "printing given table sorted by frequency: " << "\n";
+  for(i=0;i<plaintextFreq.size();i++){
+    cout << get<0>(plaintextFreq[i]) << " "
+      << get<1>(plaintextFreq[i]) << " "
+      << get<2>(plaintextFreq[i]) << endl;
 }
 
-bool RandomKeyInit::sortbyth(const tuple<int, char, float>& a, const tuple<int, char, float>& b) {
-//  return (get<2>(a) > get<2>(b));
-    return false;
+  //sum ciphertextFreq and change to percentages
+  sum=0;
+  for(i=0;i<ciphertextFreq.size();i++){
+    sum = sum + ciphertextFreq[i];
+  }
+  for (i=0;i<ciphertextFreq.size();i++){
+    ciphertextFreqPerc[i] = ciphertextFreq[i] / sum;
+  }
+
+  //Prepare ciphtertext frequeny vector
+  //<int cipher number, float cipher number freq perc
+  vector<pair<int, float>> cipher_freq_search;
+  for(i=0;i<ciphertextFreq.size();i++){
+    cipher_freq_search.push_back(make_pair(ciphertextFreq[i],ciphertextFreqPerc[i]));
+  }
+
+  //generate key
+  for(i=0;i<plaintextFreq.size();i++){
+    float j = get<2>(plaintextFreq[i]) / get<1>(plaintextFreq[i]);
+    int k = get<0>(plaintextFreq[i]);
+    for(int l=0;l<get<1>(plaintextFreq[i]);l++){
+
+      //find the closest value to J in cipher text frequencies
+      for(int m=0;m<cipher_freq_search.size();m++){
+        if(abs(j-get<1>(cipher_freq_search[m])<tempBest)){
+          tempBest=abs(j-get<1>(cipher_freq_search[m]));
+          tempBestPos=get<0>(cipher_freq_search[m]);
+        }
+
+      //put value closest to J in the putativeKey array, remove vector position
+      putativeKeyOut[tempBestPos]=k;
+      cout << "putativekey at position: " << tempBestPos << "char value at pos: " << k << endl;
+      cipher_freq_search.erase(cipher_freq_search.begin()+tempBestPos);
+      }
+
+      //reset tempBest
+      tempBest=10000;
+      tempBestPos=0;
+    }
+  }
+
+  //print putativeKey[]
+  cout << "The putative key is: " << endl;
+  for (i=0;i<106;i++){
+    cout << i << " " << putativeKeyOut[i] << endl;
+  }
+  return;
 }
